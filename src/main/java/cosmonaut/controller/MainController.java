51481d8b4ac775f1940ffa62378e821f7dde2bd2
Nezhit <92212509+Nezhit@users.noreply.github.com>
2024-03-20 21:00:48 +0300

    пофиксил по мере сил

--- src/main/java/cosmonaut/controller/MainController.java
+++ src/main/java/cosmonaut/controller/MainController.java
@@ -1,19 +1,33 @@
 package cosmonaut.controller;
 
 import cosmonaut.repository.UserRepository;
+import cosmonaut.service.FileStorageService;
 import cosmonaut.service.UserService;
 import cosmonaut.util.CurrentUserUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class MainController {
 
     private UserRepository userRepository;
+    @Autowired
+    private FileStorageService fileStorageService;
     @Autowired
     private CurrentUserUtils currentUserUtils;
     @Autowired
@@ -43,5 +57,9 @@ public class MainController {
         }
         model.addAttribute("user",currentUserUtils.getCurrentLoggedUser());
         return "personpage";
+    }
+    @GetMapping("/getAvatar/{imageName}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String imageName) {
+        return fileStorageService.getImage(imageName);
     }
 }
--- src/main/java/cosmonaut/service/FileStorageService.java
+++ src/main/java/cosmonaut/service/FileStorageService.java
@@ -1,6 +1,12 @@
 package cosmonaut.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,6 +21,15 @@ import java.util.UUID;
 public class FileStorageService {
     @Value("${file.upload-dir}")
     private String uploadDir;
+    @Value("avatars/")
+    private String imagePath;
+    private final ResourceLoader resourceLoader;
+
+
+
+    public FileStorageService(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
 
     public String storeFile(MultipartFile file) throws IOException {
         // Генерация уникального имени файла для избежания конфликтов
@@ -26,5 +41,25 @@ public class FileStorageService {
         Files.copy(file.getInputStream(), path);
 
         return fileName;
+    }
+
+    public ResponseEntity<byte[]> getImage(String imageName) {
+        try {
+
+
+            Path path = Paths.get(imagePath + imageName);
+            if (Files.exists(path)) {
+                byte[] imageData = Files.readAllBytes(path);
+                String contentType = Files.probeContentType(path);
+
+                MediaType mediaType = MediaType.parseMediaType(contentType);
+                return ResponseEntity.ok().contentType(mediaType).body(imageData);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 }
